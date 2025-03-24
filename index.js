40b08c42f90c1cf74b2ff3d01cfbc63cd84db60e
EdgarBaena <156694420+EdgarBaena@users.noreply.github.com>
2025-03-24 15:00:13 +0000

    pagina js

---
+++ index.js
@@ -0,0 +1,74 @@
+// Imprime "Hello, world!" en la consola
+console.log("Hello, world!");
+
+// Array de objetos que representan cartas, cada una con un nombre y un enlace a una imagen
+const arrayCartas = [
+  {
+    nombre: "0_blue",
+    link: "0_blue.png",
+  },
+  {
+    nombre: "0_green",
+    link: "0_green.png",
+  },
+  {
+    nombre: "0_red",
+    link: "0_red.png",
+  },
+  {
+    nombre: "0_yellow",
+    link: "0_yellow.png",
+  },
+  {
+    nombre: "1_blue",
+    link: "1_blue.png",
+  },
+  {
+    nombre: "1_green",
+    link: "1_green.png",
+  },
+  {
+    nombre: "1_red",
+    link: "1_red.png",
+  },
+  {
+    nombre: "1_yellow",
+    link: "1_yellow.png",
+  },
+  {
+    nombre: "2_blue",
+    link: "2_blue.png",
+  },
+  {
+    nombre: "2_green",
+    link: "2_green.png",
+  },
+  {
+    nombre: "2_red",
+    link: "2_red.png",
+  },
+  {
+    nombre: "2_yellow",
+    link: "2_yellow.png",
+  },
+  {
+    nombre: "3_blue",
+    link: "3_blue.png",
+  },
+  {
+    nombre: "3_green",
+    link: "3_green.png",
+  },
+  {
+    nombre: "3_red",
+    link: "3_red.png",
+  },
+  {
+    nombre: "3_yellow",
+    link: "3_yellow.png",
+  },
+  {
+    nombre: "4_blue",
+    link: "4_blue.png",
+  },
+];
